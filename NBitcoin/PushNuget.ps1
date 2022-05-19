903f2e0bb9fd9d4dec7a659529e91c10143f6665
Kiminuo <kiminuo@protonmail.com>
2022-05-19 14:35:22 +0200

    Improve PushNuget.ps1

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -1,7 +1,9 @@
+param($apiKey)
+
 rm "bin\release\" -Recurse -Force
 dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
 $package=(ls .\bin\Release\*.nupkg).FullName
-dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
+dotnet nuget push $package --source "https://api.nuget.org/v3/index.json" --api-key $apiKey
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '[^\.]*\.(\d+(\.\d+){1,3}).*', '$1')
 git tag -a "v$ver" -m "$ver"
 git push --tags
